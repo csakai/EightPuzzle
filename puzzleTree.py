37acbff99c40842d48c46a9ed96bae199cb10448
csakai <cs.chris.sakai@gmail.com>
2013-10-18 10:07:42 -0400

    started work on avl tree for cycle checking. commented out those because they are bad right now.

---
+++ puzzleTree.py
@@ -0,0 +1,18 @@
+# class puzzleTree:
+#     def __init__(self, node):
+#         self.root=node
+#         self.left=None
+#         self.right=None
+#         self.height=-1
+#         self.firstKey=node.data.B
+#         self.secondKey=node.data.H
+#         nnode=node.data.order.copy()
+#         nnode[self.firstKey]=0
+#         self.thirdKey=nnode
+
+#     def fix(self):
+#         pass
+
+#     def insert(self, node):
+#         if not self.root:
+#             self.root=node
